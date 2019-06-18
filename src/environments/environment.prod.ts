6f9dff2db4b6d158bfc3780f41eef45c3114e267
Paula Lopes Silva <pls21@msn.com>
2019-06-18 11:26:44 -0300

    README, config deploy heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.github.com'
+  apiUrl: 'https://api.github.com',
+  clientId: '693ce4d187b061cae083',
+  clientSecret: 'a06956d081ac608fae698b398d8281dce1db0103',
+  authUrl: 'https://desafio-omnibees-frontend.herokuapp.com/'
 };
