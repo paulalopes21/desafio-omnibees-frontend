e11eb87bfe6e949d55e7ad82ba44187f0134a4b2
Paula Lopes Silva <paulalopes21@users.noreply.github.com>
2019-06-18 12:03:12 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiUrl: 'https://api.github.com',
   clientId: '693ce4d187b061cae083',
   clientSecret: 'a06956d081ac608fae698b398d8281dce1db0103',
-  authUrl: 'https://github.com/login/oauth/access_token'
+  authUrl: 'https://desafio-omnibees-frontend.herokuapp.com/api/login/oauth/access_token'
+
 };
