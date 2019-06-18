b463b11b57ab37b0faaf472ef2e2d89f0071912f
Paula Lopes Silva <paulalopes21@users.noreply.github.com>
2019-06-18 13:33:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require("cors");
-app.use(cors());
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/desafio-omnibees-frontend'));
