602aaf66174670a31598e802c68f07b2f0722cf7
Paula Lopes Silva <pls21@msn.com>
2019-06-18 11:46:20 -0300

    correcao url access token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://api.github.com',
   clientId: '693ce4d187b061cae083',
   clientSecret: 'a06956d081ac608fae698b398d8281dce1db0103',
-  authUrl: 'https://desafio-omnibees-frontend.herokuapp.com/'
+  authUrl: 'https://desafio-omnibees-frontend.herokuapp.com/api/login/oauth/access_token'
 };
